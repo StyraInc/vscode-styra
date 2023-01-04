6187d79dbbd77870fb60f84387ba86515cf4bb13
Michael Sorens <msorens@styra.com>
2023-01-03 18:26:05 -0800

    cmds: modularize existing log-replay cmd
    STY-15270

--- src/lib/styra-install.ts
+++ src/lib/styra-install.ts
@@ -4,9 +4,23 @@ import * as os from "os";
 import * as fse from "fs-extra";
 import { default as fetch } from "node-fetch";
 import moveFile = require("move-file");
+import { sync as commandExistsSync } from "command-exists";
+
+// export const STYRA_CLI_CMD = 'styra2'; // TODO: for testing; do not commit!
+export const STYRA_CLI_CMD = 'styra';
 
 export class StyraInstall {
-  
+
+  static isInstalled(): boolean {
+    const styraPath = vscode.workspace
+      .getConfiguration("styra")
+      .get<string>("path");
+    const existsOnPath = commandExistsSync(STYRA_CLI_CMD);
+    const existsInUserSettings =
+      styraPath !== undefined && styraPath !== null && fs.existsSync(styraPath);
+    return existsOnPath || existsInUserSettings;
+  }
+
   static async promptForInstall(): Promise<boolean> {
     const selection = await vscode.window.showInformationMessage(
       "Styra CLI is not installed. Would you like to install it now?",
@@ -34,9 +48,8 @@ export class StyraInstall {
     const targetOS = process.platform;
     const targetArch = process.arch;
 
-    const exeFile = targetOS === "win32" ? "C:\\Program Files\\styra\\styra.exe" : "/usr/local/bin/styra";
-    // const exeFile = targetOS === "win32" ? "C:\\Program Files\\styra\\styra.exe" : "/Users/msorens/styra-test"; // TODO: do not commit this!
-    const binaryFile = targetOS === "win32" ? "styra.exe" : "styra";
+    const binaryFile = targetOS === "win32" ? STYRA_CLI_CMD + ".exe" : STYRA_CLI_CMD;
+    const exeFile = targetOS === "win32" ? "C:\\Program Files\\styra\\" + binaryFile : "/usr/local/bin/" + binaryFile;
     const tempFileLocation = os.homedir() + "/" + binaryFile;
 
     const url =
@@ -57,7 +70,6 @@ export class StyraInstall {
   private static async getBinary(url: string, tempFileLocation: string): Promise<void> {
     // adapted from https://stackoverflow.com/a/69290915
     const response = await fetch(url);
-    console.log(`starting write ${tempFileLocation}`);
     const writeStream = fse.createWriteStream(tempFileLocation, {
       autoClose: true,
       flags: "w",
