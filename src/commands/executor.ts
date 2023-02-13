63f395e3bc3e69afc05ae2d64b2402ab55aac5ae
Michael Sorens <birdwatcher42@gmail.com>
2023-02-12 19:38:41 -0800

    executor: consolidate/streamline
    STY-15609

--- src/commands/executor.ts
+++ src/commands/executor.ts
@@ -1,29 +1,41 @@
 import {checkStartup} from '../lib/utility';
-import {CommandNotifier} from '../lib/command-notifier';
 import {ICommand, ReturnValue} from '../lib/types';
-import {infoFromUserAction, teeError} from '../lib/outputPane';
+import {info, infoFromUserAction, teeError} from '../lib/outputPane';
+import {LocalStorageService, Workspace} from '../lib/local-storage-service';
 
 export class Executor {
+
+  static StorageManager = LocalStorageService.instance;
 
   static async run(command: ICommand): Promise<void> {
 
     if (!(await checkStartup())) {
       return;
     }
-    const notifier = new CommandNotifier(command.title);
-    notifier.markStart();
+    this.StorageManager.setValue(Workspace.CmdName, command.title);
+    this.announce(command.title);
 
     try {
       const result = await command.run();
       if (result === ReturnValue.Terminated) {
         infoFromUserAction(`${command.title} terminated`);
       } else {
-        notifier.markHappyFinish();
+        info(`====> ${command.title} completed`);
       }
     } catch ({message}) {
       teeError(message as string);
-      notifier.markSadFinish();
+      info(`====> ${command.title} failed`);
     }
+    this.StorageManager.setValue<string>(Workspace.CmdName, '');
+  }
+
+  static announce(name: string): void {
+    info('');
+    info('--------------------------------------------------------------------------');
+    // TODO: use https://www.npmjs.com/package/vscode-read-manifest to read commands
+    info(`Running Command: ${name}`);
+    info(`** You will see "${name} completed" (or "failed") when this is done.`);
+    info('--------------------------------------------------------------------------');
   }
 
 }
