3a302791920e52df58e5c547b51e61a1aeb2c404
Michael Sorens <birdwatcher42@gmail.com>
2023-02-12 18:29:09 -0800

    executor: more tests
    STY-15609

--- src/test-jest/commands/executor.test.ts
+++ src/test-jest/commands/executor.test.ts
@@ -1,18 +1,29 @@
 import * as utility from '../../lib/utility';
 jest.mock('../../lib/utility');
-import {CommandRunner} from '../../lib/command-runner';
 import {Executor} from '../../commands/executor';
-import {LinkConfigGit} from '../../commands/link-config-git';
-import {MultiStepInput} from '../../external/multi-step-input';
+import {ICommand, ReturnValue} from '../../lib/types';
+import {outputChannel} from '../../lib/outputPane';
 import {OutputPaneSpy} from '../utility';
+
+class MockCompletedCommand implements ICommand {
+  run = () => Promise.resolve(ReturnValue.Completed);
+  title = 'MockCommand'
+}
+
+class MockTerminatedCommand implements ICommand {
+  run = () => Promise.resolve(ReturnValue.Terminated);
+  title = 'MockCommand'
+}
+
+class MockFailedCommand implements ICommand {
+  run = () => Promise.reject('threw an error');
+  title = 'MockCommand'
+}
 
 describe('Executor', () => {
 
   const spy = new OutputPaneSpy();
-
-  beforeEach(() => {
-    jest.resetModules();
-  });
+  const spyAppendLine = jest.spyOn(outputChannel, 'appendLine');
 
   [
     [true, 'CONTINUES'],
@@ -22,16 +33,30 @@ describe('Executor', () => {
     test(`command ${description} when checkStartup returns ${succeeds}`, async () => {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       (utility.checkStartup as unknown as jest.MockInstance<any, any>).mockResolvedValue(succeeds);
-      const runnerQuietMock = jest.fn().mockResolvedValue('git@dummyUrlHere.git');
-      CommandRunner.prototype.runStyraCmdQuietly = runnerQuietMock;
-      MultiStepInput.prototype.showQuickPick = jest.fn().mockResolvedValue(({label: 'no'}));
 
-      await Executor.run(new LinkConfigGit());
+      await Executor.run(new MockCompletedCommand());
 
-      expect(runnerQuietMock.mock.calls.length).toBe(succeeds ? 1 : 0); // reached the check-git step or not
       if (succeeds) {
-        expect(spy.content).toMatch(/Styra Link Config Git terminated/);
+        expect(spyAppendLine).toHaveBeenCalled();
+        expect(spy.content).toMatch(/Running Command: MockCommand/);
+      } else {
+        expect(spyAppendLine).not.toHaveBeenCalled();
       }
+    });
+  });
+
+  [
+    {description: 'command completes', command: new MockCompletedCommand(), regex: /====> MockCommand completed/},
+    {description: 'command is terminated by user', command: new MockTerminatedCommand(), regex: /\[USER\]: MockCommand terminated/},
+    {description: 'command fails', command: new MockFailedCommand(), regex: /====> MockCommand failed/},
+  ].forEach(({description, command, regex}) => {
+    test(`reports correct info to user when ${description}`, async () => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (utility.checkStartup as unknown as jest.MockInstance<any, any>).mockResolvedValue(true);
+
+      await Executor.run(command);
+
+      expect(spy.content).toMatch(regex);
     });
   });
 
