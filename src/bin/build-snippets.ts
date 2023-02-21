261f0a4af3794b556bf7178979f1de65147f2e5f
Michael Sorens <msorens@styra.com>
2023-02-20 17:58:55 -0800

    snippet: remove the hard-coded k8s snippets
    STY-15551

--- src/bin/build-snippets.ts
+++ src/bin/build-snippets.ts
@@ -10,8 +10,8 @@ import {runStyraCmd} from '../lib-sans-vscode/command-runner';
 //       cmd line: styra link init...
 //       VSCode: "Styra Link: Initialize"
 // (3) From that styra link project's root directory run this,
-//     redirecting output to overwrite the snippets.json file in this project. Example:
-//       ts-node ../vscode-styra/src/bin/build-snippets.ts > ../vscode-styra/snippets.json
+//     redirecting output to overwrite the kubernetes.json file in this project. Example:
+//       ts-node ../vscode-styra/src/bin/build-snippets.ts > ../vscode-styra/snippets/kubernetes.json
 //     Note that this will take 6 minutes to run, which is fine for now since it is done offline.
 
 // Tips on running from the command line:
@@ -27,6 +27,7 @@ type OutputSnippetType = {
   body: string[];
   prefix: string;
   description: string;
+  scope: string;
 }
 
 async function main() {
@@ -53,11 +54,25 @@ async function generateSnippets(inputSnippets: InputSnippetType[]): Promise<{ [k
   // Neither reduce nor forEach will iterate the async operations serially; need the humble for loop to do it.
   // Running them in parallel overwhelms the system and causes an eventual timeout
   for (const {id: prefix, title, description} of inputSnippets) {
-    const result = {prefix, description, body: [await getTemplate(prefix)]};
+    const result = {prefix, description, scope: 'rego', body: [await getTemplate(prefix)]};
     snippets[title] = result;
     // eslint-disable-next-line no-console
     console.error(prefix); // report progress since it takes a couple seconds per snippet
   }
+
+  // Add a "help" snippet for good measure!
+  snippets['Styra Link intro'] = {
+    prefix: 'help_rego',
+    description: 'Introductory remarks on rego, Styra Link, and VSCode',
+    scope: 'rego',
+    body: [
+      '# Welcome to the world of rego!',
+      '# The VSCode plugin provides a wealth of ready-made snippets to jump start your rego journey.',
+      '# Note that whether you have any snippets depends on the system type of the DAS system',
+      '# that you have connected to your currently loaded VSCode project with Styra Link.',
+      '# Use the "Styra Link: Initialize" command to set that up if you you have not already.'
+    ]
+  };
   return snippets;
 }
 
