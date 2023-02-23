fc1523df517577710eaa5721c737e585c010af4f
Michael Sorens <msorens@styra.com>
2023-02-23 08:25:06 -0800

    unit-test: refinement for link-init
    STY-15714

--- src/test-jest/commands/link-init.test.ts
+++ src/test-jest/commands/link-init.test.ts
@@ -1,13 +1,40 @@
 import {CommandRunner} from '../../lib/command-runner';
+import {generatePickList} from '../../commands/utility';
 import {IDE} from '../../lib/vscode-api';
 import {LinkInit} from '../../commands/link-init';
 import {MultiStepInput} from '../../external/multi-step-input';
-jest.mock('../../lib/snippet-installer');
+import {SnippetInstaller} from '../../lib/snippet-installer';
 
 describe('LinkInit', () => {
 
   beforeEach(() => {
     IDE.getConfigValue = jest.fn().mockReturnValue(true); // getConfigValue('styra', 'debug')
+    SnippetInstaller.prototype.addSnippetsToProject = jest.fn();
+
+    // responds to query to fetch system types
+    CommandRunner.prototype.runStyraCmdQuietly = jest.fn().mockResolvedValue('["typeA", "typeB"]');
+
+    // responds to primary styra link command under test
+    CommandRunner.prototype.runStyraCmd = jest.fn().mockResolvedValue('any');
+
+    // provide responses to user inputs
+    MultiStepInput.prototype.showQuickPick = quickPickMock();
+    MultiStepInput.prototype.showInputBox = inputBoxMock();
+  });
+
+  [
+    [['link', 'init'], 'command prefix'],
+    [['--skip-git'], 'skip git initialization'],
+    [['--type', 'kubernetes'], 'system type'],
+    [['--path', 'authz'], 'authorization directory'],
+  ].forEach(([terms, description]) => {
+    test(`includes correct params for ${description}`, async () => {
+
+      await new LinkInit().run();
+
+      expect(CommandRunner.prototype.runStyraCmd).toHaveBeenCalledWith(
+        expect.arrayContaining(terms as string[]));
+    });
   });
 
   [
@@ -15,61 +42,65 @@ describe('LinkInit', () => {
     [false, 'connecting to existing system'],
   ].forEach(([useNew, description]) => {
     test(`correct params are passed for ${description}`, async () => {
-      const runnerMock = commandRunnerMock();
 
-      MultiStepInput.prototype.showQuickPick = jest.fn()
-          .mockImplementation(inputBoxMock(false, useNew as boolean));
-      MultiStepInput.prototype.showInputBox = jest.fn()
-          .mockImplementation(inputBoxMock(true, useNew as boolean));
+      MultiStepInput.prototype.showQuickPick = quickPickMock(useNew as boolean);
 
       await new LinkInit().run();
 
-      expect(runnerMock).toHaveBeenCalledWith(
-        ['link', 'init',
-          useNew ? '--create' : '--existing',
-          '--name', useNew ? 'my new DAS system' : 'my existing DAS system',
-          '--path', 'authz',
-          '--type', 'kubernetes',
-          '--skip-git'
-        ]);
+      expect(CommandRunner.prototype.runStyraCmd).toHaveBeenCalledWith(
+        expect.arrayContaining(
+          useNew ? ['--create', '--name', 'my new DAS system']
+            : ['--existing', '--name', 'my existing DAS system']
+        ));
     });
   });
 
-  const commandRunnerMock = () => {
-    // responds to query to fetch system types
-    const runnerQuietMock = jest.fn().mockResolvedValue('["typeA", "typeB"]');
-    CommandRunner.prototype.runStyraCmdQuietly = runnerQuietMock;
-    // responds to primary styra link command
-    const runnerMock = jest.fn().mockResolvedValue('some info here');
-    CommandRunner.prototype.runStyraCmd = runnerMock;
-    return runnerMock;
-  };
+  test('uses dynamic system type list to populate user prompt', async () => {
+    await new LinkInit().run();
 
-  // this mock robustly makes tests independent of the order of prompts
-  const inputBoxMock = (isInputBox: boolean, isNew = true) => ({prompt, placeholder}: {prompt: string, placeholder: string}) => {
-    let result: string;
-    const target = isInputBox ? prompt : placeholder;
-    switch (target.replace(/ \(.*\)/, '')) { // ignore trailing parenthetical, if any
+    expect(MultiStepInput.prototype.showQuickPick).toHaveBeenCalledWith(
+      expect.objectContaining({items: generatePickList(['typeA', 'typeB'])})
+    );
+  });
 
-      case 'Create a new DAS system or connect with an existing one?':
-        result = isNew ? 'create new DAS system' : 'connect with existing DAS system';
-        break;
-      case 'Choose a unique name for the DAS System':
-        result = 'my new DAS system';
-        break;
-      case 'Enter the name of an existing DAS System':
-        result = 'my existing DAS system';
-        break;
-      case 'Pick a system type':
-        result = 'kubernetes';
-        break;
-      case 'Where should policies be stored in the project?':
-        result = 'authz';
-        break;
-      default:
-        result = 'UNKNOWN'; // should never happen
-    }
-    return isInputBox ? result : {label: result};
-  };
+  test('adds snippets after setting up system', async () => {
+    await new LinkInit().run();
 
+    expect(SnippetInstaller.prototype.addSnippetsToProject).toHaveBeenCalled();
+  });
+
+  // The `_inputMock` robustly makes tests independent of the order of prompts.
+  // It handles both showQuickPick (which uses `placeholder`) and showInputBox (which uses `prompt`).
+  // When reusing this, you should always have `isInputBox` to distinguish showQuickPick/showInputBox.
+  // Unlike link-config-git.test, not using InputMockOptions because only one option (`isNew`) to manage.
+
+  const inputBoxMock = () => _inputMock(true, false /* don't care term */);
+  const quickPickMock = (isNew = true) => _inputMock(false, isNew);
+
+  const _inputMock = (isInputBox: boolean, isNew: boolean) =>
+    jest.fn().mockImplementation(
+      ({prompt, placeholder}: { prompt: string, placeholder: string }) => {
+        let result: string;
+        const target = (isInputBox ? prompt : placeholder).replace(/\s*\(.*\)/, ''); // ignore trailing parenthetical, if any
+        switch (target) {
+          case 'Create a new DAS system or connect with an existing one?':
+            result = isNew ? 'create new DAS system' : 'connect with existing DAS system';
+            break;
+          case 'Choose a unique name for the DAS System':
+            result = 'my new DAS system';
+            break;
+          case 'Enter the name of an existing DAS System':
+            result = 'my existing DAS system';
+            break;
+          case 'Pick a system type':
+            result = 'kubernetes';
+            break;
+          case 'Where should policies be stored in the project?':
+            result = 'authz';
+            break;
+          default:
+            result = 'UNKNOWN'; // should never happen
+        }
+        return isInputBox ? result : {label: result};
+      });
 });
