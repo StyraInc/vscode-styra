867766e3618516e0926bf048685941f9a8cc4eb9
Michael Sorens <birdwatcher42@gmail.com>
2023-03-23 17:06:27 -0700

    coverage: refine badge insertion tool
    STY-15924

--- src/bin/update-coverage.ts
+++ src/bin/update-coverage.ts
@@ -1,21 +1,39 @@
 import * as fs from 'fs';
 
+// Purpose: Update the code coverage badge in the README.md file.
+
 const COVERAGE_FILE = 'coverage/coverage-summary.json';
 const README_FILE = 'README.md';
-const TEMPLATE = 'https://img.shields.io/badge/Coverage-';
+const BADGE_URL_REGEX = /https:\/\/img.shields.io\/badge\/Coverage-\d+%25-\w+/;
+const BADGE_TEMPLATE = 'https://img.shields.io/badge/Coverage-<coverage>%25-<color>';
 
-function replaceFileContent(filePath:string, placeholder: RegExp, newText: string) {
+enum Threshold {
+  Critical = 50,
+  Warning = 75
+}
+
+function updateCoverageValue(filePath:string, regexp: RegExp, newURL: string) {
   const fileContent = fs.readFileSync(filePath, 'utf8');
-  const newContent = fileContent.replace(placeholder, newText);
+  const newContent = fileContent.replace(regexp, newURL);
   fs.writeFileSync(filePath, newContent);
 }
 
-// cat coverage/coverage-summary.json | jq '.total.lines.pct | round'
-function getCoverage() {
+function getCoverage(): number {
   const data = fs.readFileSync(COVERAGE_FILE, 'utf8');
   const parsed = JSON.parse(data);
   return Math.round(parsed.total.lines.pct);
 }
 
-// TODO: Update colors, too!
-replaceFileContent(README_FILE, new RegExp(`${TEMPLATE}\\d+`), `${TEMPLATE}${getCoverage()}`);
+function genCoverageUrl(coverage: number): string {
+  return BADGE_TEMPLATE
+    .replace('<coverage>', coverage.toString())
+    .replace('<color>', getCoverageColor(coverage));
+}
+
+function getCoverageColor(coverage: number): string {
+  // see https://shields.io/ for possible color names
+  return coverage < Threshold.Critical ? 'red' : coverage < Threshold.Warning ? 'yellow' : 'brightgreen';
+
+}
+
+updateCoverageValue(README_FILE, BADGE_URL_REGEX, genCoverageUrl(getCoverage()));
