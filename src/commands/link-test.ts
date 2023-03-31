28963ce1795c5586d7e22145ff85f5e636d71bad
Michael Sorens <birdwatcher42@gmail.com>
2023-03-30 20:44:52 -0700

    bonus: unit test catchup on link-test
    Also add missing --output functionality for link-test
    
    STY-16021

--- src/commands/link-test.ts
+++ src/commands/link-test.ts
@@ -1,13 +1,21 @@
 import {CommandRunner} from '../lib/command-runner';
 import {ICommand, ReturnValue} from '../lib/types';
+import {IDE} from '../lib/vscode-api';
 import {info} from '../lib/output-pane';
+import {Setting} from '../lib/ide-settings';
 
 export class LinkTest implements ICommand {
 
   title = 'Styra Link Test';
 
   async run(): Promise<ReturnValue> {
-    const result = await new CommandRunner().runStyraCmd(['link', 'test']);
+
+    const styraArgs = ['link', 'test'];
+    const outputFormat = IDE.getConfigValue<string>('styra', Setting.Format);
+    if (outputFormat) {
+      styraArgs.push('--output', outputFormat.toLowerCase());
+    }
+    const result = await new CommandRunner().runStyraCmd(styraArgs);
     info(result);
     return ReturnValue.Completed;
   }
